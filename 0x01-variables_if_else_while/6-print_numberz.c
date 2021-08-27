286317a6186373c2719836985f9246f3f685255b
Bruhan-Makong <makongbruhan70@gmail.com>
2021-08-27 06:14:36 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a predefined keyword or function in C
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
